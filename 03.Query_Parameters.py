50277a4f4c80731a8ffa8ebcb13596ad3782d221
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2026-09-22 17:21:26 +0530

    Update 03.Query_Parameters.py

--- 03.Query_Parameters.py
+++ 03.Query_Parameters.py
@@ -8,7 +8,7 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items")
+@app.get("/items/")
 def get_items(skip:int = 0 ,limit:int = 10):
     return fake_items_db[skip:skip+limit]
 
