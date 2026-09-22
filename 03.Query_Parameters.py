54b58805bfe4ca95181d21f0139263ab13b753ba
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2026-09-22 17:20:40 +0530

    Create 03.Query_Parameters.py

---
+++ 03.Query_Parameters.py
@@ -0,0 +1,20 @@
+# Query Parameters - Following the Official FastAPI Tutorial
+# Learn how to handle optional parameters in URLs
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+def get_items(skip:int = 0 ,limit:int = 10):
+    return fake_items_db[skip:skip+limit]
+
+@app.get("/items/{item_id}")
+def get_item_by_id(item_id:str,q:str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
